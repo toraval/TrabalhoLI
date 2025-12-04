749992f58c5b522376f9f6da62fe19b585645a50
ryan <ryanpossanrubi@gmail.com>
2025-12-04 17:27:49 +0000

    base de dados antonio ta com preguiça

--- 1outros/DataBase/Dump20251201 (1).sql
+++ 1outros/DataBase/Dump20251201 (1).sql
@@ -19,6 +19,9 @@
 -- Table structure for table `despesas`
 --
 
+CREATE DATABASE mydb;
+USE mydb;
+
 DROP TABLE IF EXISTS `despesas`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!50503 SET character_set_client = utf8mb4 */;
