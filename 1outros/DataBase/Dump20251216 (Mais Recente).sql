0e2693bdcb3a39e8d983699786e8d3e74f857672
unknown <sebastianmsimoes@ua.pt>
2025-12-17 12:46:25 +0000

    base dados pronta para importar

--- 1outros/DataBase/Dump20251216 (Mais Recente).sql
+++ 1outros/DataBase/Dump20251216 (Mais Recente).sql
@@ -3,6 +3,7 @@
 -- Host: 127.0.0.1    Database: mydb
 -- ------------------------------------------------------
 -- Server version	8.4.3
+use mydb;
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
